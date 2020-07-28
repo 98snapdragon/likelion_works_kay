81ea00ed47dfa112a2e4132ccec84b9a2e7612de
user <98snadpragon@gmail.com>
2020-07-28 20:03:06 +0900

    django AddAuthor

--- django CRUD/django 1-2주차 과제/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\user\desktop\likelion\django\django 과제\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
